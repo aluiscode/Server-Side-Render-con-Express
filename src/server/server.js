682044103e4ff74c1d5e1d31aface085233d8399
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-01 16:17:41 -0500

    Clase 10 -Definición de la función principal para realizar el renderizado desde el servidor

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { renderRoutes } from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
+import reducer from '../frontend/reducers';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import initialState from '../frontend/initialState';
 
 const app = express();
 dotenv.config();
@@ -21,20 +30,36 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.get('*', (req, res) => {
-  res.send(`
+const setResponse = (html) => {
+  return (`
   <!DOCTYPE html>
-  <html>
-    <head>
-      <link rel="stylesheet" href="assets/app.css" type="text/css">
-      <title>Platzi Video</title>
-    </head>
-    <body>
-      <div id="app"></div>
-      <script src="assets/app.js" type="text/javascript"></script>
-    </body>
-  </html>`);
-});
+    <html>
+      <head>
+        <link rel="stylesheet" href="assets/app.css" type="text/css">
+        <title>Platzi Video</title>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+        <script src="assets/app.js" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        { renderRoutes(serverRoutes) }
+      </StaticRouter>
+    </Provider>,
+  );
+
+  res.send(setResponse(html));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   err ? console.log(err) : console.log(`Server is running on port ${PORT}`);
